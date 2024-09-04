624748dc4a04173621efac6fa8fcb31b79c96c38
DhinarArum06 <dhinar.arum@gmail.com>
2024-09-04 11:21:30 +0700

    Biodata Dhinar

---
+++ BiodataDhinar.java
@@ -0,0 +1,19 @@
+/**
+ * BiodataDhinar
+ */
+public class BiodataDhinar{
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("NAMA    : DHINAR ARUM DAMAYANTI");
+        System.out.println("NIM     : 244107020077");
+        System.out.println("KELAS   : 1B");
+        System.out.println("NO ABSEN: 08");
+        System.out.println("PRODI   : TEKNIK INFORMATIKA");
+        System.out.println("ALAMAT  : METLANG CIBITUNG CLUSTER TAMAN SENAYAN BLOK G5 NO. 21");
+        System.out.println("NO HP   : 081212221608");
+        System.out.println("HOBI    : MEMBACA DAN MENDENGARKAN MUSIK");
+    }
+}
