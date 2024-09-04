d8530a14ba6fdd4ee4f8a1129698d8e05709d679
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2024-09-04 11:14:24 +0700

    Commit Biodata Wahyu

---
+++ Biodatawahyu.java
@@ -0,0 +1,19 @@
+/**
+ * Biodatawahyu
+ */
+public class Biodatawahyu {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("NAMA    : WAHYUDI SATRIAWAN HAMID");
+        System.out.println("NIM     : 244107020137");
+        System.out.println("KELAS   : 1B");
+        System.out.println("NO ABSEN: 26");
+        System.out.println("PRODI   : TEKNIK INFORMATIKA");
+        System.out.println("ALAMAT  : WLINGI, BLITAR");
+        System.out.println("NO HP   : 081298709495");
+        System.out.println("HOBI    : TIDUR");
+    }
+}
